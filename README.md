9c1438dab33d06a65e0655370a8d9c973fbeb5a1
isadia-brian <developerisadia@gmail.com>
2024-11-24 02:10:58 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Next Template
+
+<a href="https://idx.google.com/new?template=https://github.com/isadia-brian/idx-next-template">
+  <img height="32" alt="Open in IDX" src="https://cdn.idx.dev/btn/try_dark_32.svg">
+</a>
---
+++ idx-template.nix
@@ -0,0 +1,23 @@
+{ pkgs, version ? "latest", importAlias ? "@/*", language ? "ts"
+, packageManager ? "npm", srcDir ? false, eslint ? false, app ? false
+, tailwind ? false, ... }: {
+
+  packages = [ pkgs.nodejs_20 pkgs.yarn pkgs.pnpm ];
+
+  bootstrap = ''
+    mkdir -p "$WS_NAME"
+	npx create-next-app@${version} "$WS_NAME" \
+		--skip-install \
+		--import-alias=${importAlias} \
+		--${language} \
+		${if eslint then "--eslint" else "--no-eslint"} \
+		${if srcDir then "--src-dir" else "--no-src-dir"} \
+		${if app then "--app" else "--no-app"} \
+		${if tailwind then "--tailwind" else "--no-tailwind"}
+	mkdir -p "$WS_NAME/.idx/"
+	cp -rf ${./dev.nix} "$WS_NAME/.idx/dev.nix"
+	chmod -R +w "$WS_NAME"
+	mv "$WS_NAME" "$out"
+	cd "$out"; npm install --package-lock-only --ignore-scripts
+  '';
+}
